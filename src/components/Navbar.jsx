f83a9d78a3723116355f13347b2f54d3182db0d5
Sumaira Ameen <sumairaameen0001@gmail.com>
2025-04-28 15:12:13 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function BasicExample() {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
     <Container>
-      <Navbar.Brand href="#home">Post App</Navbar.Brand>
+      <Navbar.Brand href="#home">Postlify</Navbar.Brand>
       <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#about">About</Nav.Link>
